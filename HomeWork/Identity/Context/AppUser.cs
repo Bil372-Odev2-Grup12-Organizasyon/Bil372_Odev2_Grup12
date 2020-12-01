d88aa5743d90c21018f94fcbb2fd68eccf4d320c
Cenk Gokturk <37985483+cenkgokturk@users.noreply.github.com>
2020-12-01 19:26:54 +0300

    Tables related to users have been updated

---
+++ HomeWork/Identity/Context/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Context
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string PictureUrl { get; set; }
+        public string Gender { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+
+        public List<Conference> Conferences { get; set; }
+        //public List<Submissions> Submissions { get; set; }
+    }
+}
---
+++ HomeWork/Identity/Context/UserLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Context
+{
+    public class UserLog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Primary_Email { get; set; }
+        public string Secondary_Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string UrlAddress { get; set; }
+
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime CreateionDate { get; set; }
+    }
+}
