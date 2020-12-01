ee13eda3a2ae792aab9d02e83e8e1ef5ade53fa6
Cenk Gokturk <37985483+cenkgokturk@users.noreply.github.com>
2020-12-01 21:12:55 +0300

    CRUD operations for roles

---
+++ HomeWork/Identity/Models/AddRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Models
+{
+    public class AddRoleViewModel
+    {
+        [Required(ErrorMessage = "Ad alanı gereklidir.")]
+        [Display(Name = "Rol Adı : ")]
+        public string RoleName { get; set; }
+    }
+}
---
+++ HomeWork/Identity/Models/RoleAssignViewmodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Models
+{
+    public class RoleAssignViewmodel
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Exist { get; set; }
+    }
+}
