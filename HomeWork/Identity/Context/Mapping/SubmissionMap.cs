2252e5efc835aa7bfbc4f1d073b2d52bd347db3c
elcinduman <elcinduman06@gmail.com>
2020-12-02 16:15:16 +0300

    Necessary front end files and mapping for submissions are added.

---
+++ HomeWork/Identity/Context/Mapping/SubmissionMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Context.Mapping
+{
+    public class SubmissionMap : IEntityTypeConfiguration<Submissions>
+    {
+        public void Configure(EntityTypeBuilder<Submissions> builder)
+        {
+            builder.HasKey(I => I.SubmissionID);
+            builder.Property(I => I.SubmissionID).UseIdentityColumn();
+
+            
+            builder.HasOne(I => I.Conference).WithMany(I => I.Submissions).HasForeignKey(I => I.ConfId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
