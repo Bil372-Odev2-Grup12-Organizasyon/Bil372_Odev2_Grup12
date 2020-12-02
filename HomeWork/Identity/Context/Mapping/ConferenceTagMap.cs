7493e2115c19211bb0d58bbefc894af7015d0bd5
elcinduman <elcinduman06@gmail.com>
2020-12-02 11:58:16 +0300

    Mappings are added

---
+++ HomeWork/Identity/Context/Mapping/ConferenceTagMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Context.Mapping
+{
+    public class ConferenceTagMap : IEntityTypeConfiguration<ConferenceTags>
+    {
+        public void Configure(EntityTypeBuilder<ConferenceTags> builder)
+        {
+
+            builder.HasKey(I => I.Id);
+            builder.Property(I => I.Id).UseIdentityColumn();
+            builder.Property(x => x.Tags).HasMaxLength(100).IsRequired();
+
+            builder.HasOne(I => I.Conference).WithMany(I => I.ConferenceTags).HasForeignKey(I => I.ConfID).OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
