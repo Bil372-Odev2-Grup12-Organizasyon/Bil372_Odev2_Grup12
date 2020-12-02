836bb555722ecdbe6cb68ab686036ad374adffea
Elcin <59923068+elcinduman@users.noreply.github.com>
2020-12-02 22:16:41 +0300

    Submission files are added

---
+++ HomeWork/Identity/Migrations/20201202180246_addIsactiveColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.Migrations
+{
+    public partial class addIsactiveColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Submissions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Submissions");
+        }
+    }
+}
---
+++ HomeWork/Identity/Migrations/20201202181547_addSubmissionColumn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.Migrations
+{
+    public partial class addSubmissionColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Submission",
+                table: "Submissions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Submission",
+                table: "Submissions");
+        }
+    }
+}
