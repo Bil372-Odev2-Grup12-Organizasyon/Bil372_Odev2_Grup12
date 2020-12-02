10ab85d8b8c6390beef87716a68f8d8d3042fff3
Cenk Gokturk <37985483+cenkgokturk@users.noreply.github.com>
2020-12-02 16:48:19 +0300

    Table for submissions has been added

---
+++ HomeWork/Identity/Context/Submissions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Context
+{
+    public class Submissions
+    {
+
+        public int SubmissionID { get; set; }
+
+        public int prevSubmissionID { get; set; }
+
+        public int ConfId { get; set; }
+        public Conference Conference { get; set; }
+
+
+    }
+}
