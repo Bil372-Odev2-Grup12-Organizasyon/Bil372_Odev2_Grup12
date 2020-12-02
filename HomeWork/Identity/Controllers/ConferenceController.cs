52cd9d59289e9b04250b79f6bbf9819df9602be9
elcinduman <elcinduman06@gmail.com>
2020-12-02 15:57:02 +0300

    Conference and developer controllers are added.

---
+++ HomeWork/Identity/Controllers/ConferenceController.cs
@@ -0,0 +1,22 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
